d5d0ad8e3f425f4bc1efb1c0535952c4a7ec34d0
richardlofi@gmail.com <richardlofi@gmail.com>
2019-12-23 16:14:16 +0000

    added post loading

--- wp-content/themes/p4nd4/index.php
+++ wp-content/themes/p4nd4/index.php
@@ -1,4 +1,12 @@
 <?php
+if ( get_query_var('p') ) {
+    $p = get_query_var('p');
+
+    $current_post = get_post( $p );
+    //var_dump($current_post);
+}
+
+
 get_header();
 ?>
 
@@ -17,13 +25,34 @@ get_header();
                 <li class="active">
                     <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Home</a>
                     <ul class="collapse list-unstyled" id="homeSubmenu">
-                        <li><a href="#">Home 1</a></li>
                         <li><a href="#">Home 2</a></li>
                         <li><a href="#">Home 3</a></li>
                     </ul>
                 </li>
 
                 <li><a href="#">About</a></li>
+
+<?php
+$args = array(
+'post_type'=> 'post',
+'orderby'    => 'ID',
+'post_status' => 'publish',
+'order'    => 'ASC',
+'posts_per_page' => -1 // this will retrive all the post that is published
+);
+$result = new WP_Query( $args );
+
+
+if ( $result->have_posts() ) :
+        while ( $result->have_posts() ) : $result->the_post(); ?>
+            <li><a href="<?php the_permalink() ?>"><?php the_title()  ?></a></li>
+        <?php endwhile;
+
+endif;
+?>
+
+
+
         </nav>
         <!-- Sidebar End -->
 
@@ -57,49 +86,42 @@ get_header();
 
 
 
+		<div class= 'narrow-page-container'>
+            		<h2>  
+			<?php echo( $current_post->post_title ); ?>
+			</h2>
+	    		<p>
+			<?php echo( $current_post->post_content ); ?>
+	    		</p>
+            		
+			<div class="line"></div>
+			<h6>
+<?php
+  echo( "ID : " . $current_post->ID . "<br/>" );
+  echo( "author : " . $current_post->post_author . "<br/>" );
+  echo( "date : " . $current_post->post_date . "<br/>" );
+  echo( "gmt : " . $current_post->post_date_gmt . "<br/>" );
+  echo( "excerpt : " . $current_post->post_excerpt . "<br/>" );
+  echo( "status : " . $current_post->post_status . "<br/>" );
+  echo( "comment status : " . $current_post->comment_status . "<br/>" );
+  echo( "ping status : " . $current_post->ping_status . "<br/>" );
+  echo( "name : " . $current_post->post_name . "<br/>" );
+  echo( "parent : " . $current_post->post_parent . "<br/>" );
+  echo( "guid : " . $current_post->guid . "<br/>" );
+  echo( "menu : " . $current_post->menu_order . "<br/>" );
+  echo( "type : " . $current_post->post_type . "<br/>" );
+  echo( "mime type : " . $current_post->post_mime_type . "<br/>" );
+  echo( "comment count : " . $current_post->comment_count . "<br/>" );
+  echo( "filter : " . $current_post->filter . "<br/>" );
 
-            <h2>Collapsible Sidebar Using Bootstrap 4</h2>
-	    <p>Some Page Content</p>
-            <div class="line"></div>
 
+?>
+			</h6>
+		</div>
         </div>
         <!-- Content ENDS -->
 
     </div>
-
-
-
-
-<!--
-<?php
-$args = array(
-'post_type'=> 'post',
-'orderby'    => 'ID',
-'post_status' => 'publish',
-'order'    => 'ASC',
-'posts_per_page' => -1 // this will retrive all the post that is published
-);
-$result = new WP_Query( $args );
-
-
-if ( $result->have_posts() ) :
-        while ( $result->have_posts() ) : $result->the_post(); ?>
-                <h3><?php the_title()?></h3>
-        <?php endwhile;
-
-else :
-        echo '<p>There are no posts!</p>';
-
-endif;
-?>
--->
-
-
-
-
-
-
-
 
 
 
