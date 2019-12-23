99f107ea661e77af516baab823df0ff4c836c981
richardlofi@gmail.com <richardlofi@gmail.com>
2019-12-23 15:15:34 +0000

    added cookies JS

--- wp-content/themes/p4nd4/header.php
+++ wp-content/themes/p4nd4/header.php
@@ -3,6 +3,11 @@
 
 <head>
 
+	<!-- call Civic JS --> 
+	<script src="https://cc.cdn.civiccomputing.com/8/cookieControl-8.x.min.js"></script>
+	<script src="<?php echo(get_template_directory_uri() . '/civic_cookies.js' );?>"></script>
+
+
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
