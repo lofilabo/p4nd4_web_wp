8f2df880836a2c604aa62759c7e25c568ff1341e
richardlofi@gmail.com <richardlofi@gmail.com>
2019-12-23 14:05:51 +0000

    bootstrap implemented

---
+++ wp-content/themes/p4nd4/header.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link href="favicon.ico" rel="icon" type="image/x-icon" />
+    <title>Collapsible sidebar using Bootstrap 4</title>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+
+ 
+         <link rel="stylesheet" href="<?php echo(get_template_directory_uri() . '/style.css' );?>");
+
+    <!-- Font Awesome JS -->
+    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/solid.js" integrity="sha384-tzzSw1/Vo+0N5UhStP3bvwWPq+uvzCMfrN1fEFe+xBmv1C/AtVX5K0uZtmcHitFZ" crossorigin="anonymous"></script>
+    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/fontawesome.js" integrity="sha384-6OIrr52G08NpOFSZdxxz1xdNSndlD4vdcf/q2myIUVO0VsqaGHJsB0RaBE01VTOY" crossorigin="anonymous"></script>
+</head>
+
+<body>
+
+
+
--- wp-content/themes/p4nd4/index.php
+++ wp-content/themes/p4nd4/index.php
@@ -1,23 +1,109 @@
 <?php
+get_header();
+?>
 
+
+   <div class="wrapper">
+
+        <!-- Sidebar  -->
+        <nav id="sidebar">
+            <div class="sidebar-header">
+                <h3>p4nd4</h3>
+            </div>
+
+            <ul class="list-unstyled components">
+                <p>All Articles</p>
+
+                <li class="active">
+                    <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Home</a>
+                    <ul class="collapse list-unstyled" id="homeSubmenu">
+                        <li><a href="#">Home 1</a></li>
+                        <li><a href="#">Home 2</a></li>
+                        <li><a href="#">Home 3</a></li>
+                    </ul>
+                </li>
+
+                <li><a href="#">About</a></li>
+        </nav>
+        <!-- Sidebar End -->
+
+
+
+        <!-- Content  -->
+        <div id="content">
+
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <div class="container-fluid">
+
+                    <button type="button" id="sidebarCollapse" class="btn btn-info">
+                        <i class="fas fa-align-left"></i>
+                        <span>Toggle Sidebar</span>
+                    </button>
+                    <button class="btn btn-dark d-inline-block d-lg-none ml-auto" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <i class="fas fa-align-justify"></i>
+                    </button>
+
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="nav navbar-nav ml-auto">
+                            <li class="nav-item active"><a class="nav-link" href="#">Page</a></li>
+                            <li class="nav-item active"><a class="nav-link" href="#">Page</a></li>
+                            <li class="nav-item active"><a class="nav-link" href="#">Page</a></li>
+                            <li class="nav-item active"><a class="nav-link" href="#">Page</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+
+
+
+
+
+            <h2>Collapsible Sidebar Using Bootstrap 4</h2>
+	    <p>Some Page Content</p>
+            <div class="line"></div>
+
+        </div>
+        <!-- Content ENDS -->
+
+    </div>
+
+
+
+
+<!--
+<?php
 $args = array(
 'post_type'=> 'post',
 'orderby'    => 'ID',
 'post_status' => 'publish',
 'order'    => 'ASC',
-'posts_per_page' => -1 // this will retrive all the post that is published 
+'posts_per_page' => -1 // this will retrive all the post that is published
 );
 $result = new WP_Query( $args );
 
- 
+
 if ( $result->have_posts() ) :
-	while ( $result->have_posts() ) : $result->the_post(); ?>
-		<h2><?php the_title()?></h2> 
-	<?php endwhile;
- 
+        while ( $result->have_posts() ) : $result->the_post(); ?>
+                <h3><?php the_title()?></h3>
+        <?php endwhile;
+
 else :
-	echo '<p>There are no posts!</p>';
- 
+        echo '<p>There are no posts!</p>';
+
 endif;
- 
+?>
+-->
+
+
+
+
+
+
+
+
+
+
+
+<?php 
+get_footer();
 ?>
